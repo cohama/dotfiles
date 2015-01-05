edf9f28481b23fb0351de5da3075ed577cbf6ec3
cohama <cohama@live.jp>
2015-01-05 14:34:51 +0900

    Launch LilyTerm with tmux

--- xmonad.hs
+++ xmonad.hs
@@ -27,7 +27,7 @@ import Data.Ratio ((%))
 -- The preferred terminal program, which is used in a binding below and by
 -- certain contrib modules.
 --
-myTerminal = "lilyterm"
+myTerminal = "lilyterm -e tmux -2"
 
 -- Whether focus follows the mouse pointer.
 myFocusFollowsMouse :: Bool
