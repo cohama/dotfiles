1e70b5d09e0a19e44c516195296fbb406f9bd915
cohama <cohama@live.jp>
2020-07-26 17:06:35 +0900

    Add sxiv config

--- symln.sh
+++ symln.sh
@@ -17,3 +17,7 @@ ln -Fsi ~/dotfiles/termite.config ~/.config/termite/config
 # twmn
 mkdir -p ~/.config/twmn
 ln -Fsi ~/dotfiles/twmn.conf ~/.config/twmn
+
+# sxiv
+mkdir -p ~/.config/sxiv/exec
+ln -Fsi ~/dotfiles/sxiv-key-hander ~/.config/sxiv/exec/key-handler
